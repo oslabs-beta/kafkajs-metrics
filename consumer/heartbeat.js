02cfaa3e0d15fb3ba7fc29b38c4fb5ad59e7b7e3
Rebecca Anderson <randersonviolin@gmail.com>
2023-02-25 12:38:34 -0500

    heartbeatOn function written

---
+++ consumer/heartbeat.js
@@ -0,0 +1,32 @@
+// method: heartbeatOn - console logs each heartbeat with the consumer name
+// method: heartbeatAlert(millisseconds) - console.logs an ALERT (eventually set this to LOG?) if the heartbeat extends inputted num of milliseconds (should be longer than heartbeat, less than setInterval)
+// need to keep track of most recent heartbeat - should this be saved to consumer metrics obj?
+// should we turn this off is the consumer disconnects? Or this alert should just be in addition to consumer disconnecting?
+
+const heartbeatMetrics = {
+  lastHeartbeat: null,
+  lastHeartbeatInterval: null,
+};
+
+// this function console logs every heartbeat with an optional parameter of a consumer identifying name, otherwise memberId is used
+function heartbeatOn(consumerIdentifyingName) {
+  // does this need to be consumer.events.heartbeat? Different in endBatchProcess (no events)
+  consumer.on("consumer.events.heartbeat", (e) => {
+    lastHeartbeatInterval = e.timestamp - lastHeartbeat;
+    heartbeatMetrics.lastHeartbeat = e.timestamp;
+    const consumerId = consumerIdentifyingName || e.memberId;
+    console.log(`${consumerId} emits heartbeat at ${e.timestamp}`);
+  });
+}
+
+// this function allows a dev to input max milliseconds limit for heartbeatInterval at which a breakpoin alert will be console logged
+
+function heartbeatAlert(
+  maxMilliseconds,
+  howFrequentlyToCheck,
+  consumerIdentifyingName
+) {
+  // how to piggy back this on the previous function? use a setInterval timer
+  // is setInterval timer similar to heartbeatOn in that once it is set, it just keeps going?
+  // how to easily turn off? save variable in heartbeat Metrics?
+}
