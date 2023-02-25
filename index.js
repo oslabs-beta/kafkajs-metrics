17550d8e0d11bfe72f49ce1a0537dc1c7ece3f64
Paul Davey <118549898+pauldavey1@users.noreply.github.com>
2023-02-25 16:41:01 -0500

    implement metricize(client) functionality

--- index.js
+++ index.js
@@ -4,9 +4,18 @@ const consumerMetricize = require('./consumer');
 // global metrics object here? TBD
 const metrics = {};
 
-// need to create switch case function to call correct metricize function for consumer/producer/admin – for now, just consumer will work
-function metricize(consumer) {
-  consumerMetricize(consumer);
+// metricize kafka client
+function metricize(client) {
+  // create client.metrics property for global metrics
+  client.metrics = {};
+
+  // metricize consumer constructor
+  const vanillaConsumer = client.consumer;
+  client.consumer = function () {
+    return consumerMetricize(vanillaConsumer.apply(this, arguments));
+  };
+
+  // need to do same for producer & admin
 }
 
 module.exports = { metrics, metricize };
