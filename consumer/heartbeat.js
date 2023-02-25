11f44771a815b5dc6c399c8e967e35c3801e0c12
Rebecca Anderson <randersonviolin@gmail.com>
2023-02-25 16:44:29 -0500

    working heartbeatOn method

--- consumer/heartbeat.js
+++ consumer/heartbeat.js
@@ -1,27 +1,32 @@
 // method: heartbeatOn - console logs each heartbeat with the consumer name
 // method: heartbeatAlert(millisseconds) - console.logs an ALERT (eventually set this to LOG?) if the heartbeat extends inputted num of milliseconds (should be longer than heartbeat, less than setInterval)
-// need to keep track of most recent heartbeat - should this be saved to consumer metrics obj?
-// should we turn this off is the consumer disconnects? Or this alert should just be in addition to consumer disconnecting?
+//Questions: this alert would be in addition to a disconnect alert, if one is fired. Should disconnect event override/cancel the heartbeat event?
+// if disconnect or stop or crash event is emitted, should heartbeatMetrics be reset to 0? Should heartbeatAlert be cancelled?
 
 const heartbeatMetrics = {
   lastHeartbeat: 0,
   lastHeartbeatInterval: 0,
 };
 
-// this function console logs every heartbeat with an optional parameter of a consumer identifying name, otherwise memberId is used
+// this function console logs every heartbeat with an optional parameter of a consumer identifying name, otherwise payload memberId is used
 function heartbeatOn(consumerIdentifyingName) {
   // does this need to be consumer.events.heartbeat? Different in endBatchProcess (no events)
 
-  consumer.on("consumer.events.heartbeat", (e) => {
+  consumer.on("consumer.heartbeat", (e) => {
     if (heartbeatMetrics.lastHeartbeat) {
       heartbeatMetrics.lastHeartbeatInterval =
         e.timestamp - heartbeatMetrics.lastHeartbeat;
     }
     heartbeatMetrics.lastHeartbeat = e.timestamp;
-    const consumerId = consumerIdentifyingName || e.memberId;
+    // console.log(e.payload.memberId)
+    const consumerId = consumerIdentifyingName || e.payload.memberId;
     console.log(`${consumerId} emits heartbeat at ${e.timestamp}`);
+    console.log("heartbeatMetrics", heartbeatMetrics);
   });
 }
+
+consumer.heartbeatTurnOn = heartbeatOn;
+consumer.heartbeatTurnOn();
 
 // this function allows a dev to input max milliseconds limit for heartbeatInterval at which a breakpoin alert will be console logged
 
