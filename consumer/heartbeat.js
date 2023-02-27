83b7a9c28d96973a6866324cacc7556d0b118fe8
Rebecca Anderson <randersonviolin@gmail.com>
2023-02-27 17:33:52 -0500

    fully functional single-function-export heartbeat

--- consumer/heartbeat.js
+++ consumer/heartbeat.js
@@ -1,83 +1,84 @@
-const heartbeat = {};
 
-heartbeat.heartbeatMetrics = {
-  lastHeartbeat: 0,
-  lastHeartbeatDuration: 0,
-  longestHeartbeatDuration: 0,
-};
+// Overall functionality:
+  // provides constantly updated static property values for lastHeartbeat timestamp, lastHeartbeatDuration, longestHeartbeatDuration
+  // provides methods to turn on/off console log for each heartbeat event emitted
+  // provides methods to set/turn off breakpoint alert message when lastHeartbeatDuration exceeds inputted time interval
+  // TODO: Consider accessing saved consumer name for console logs
 
-heartbeat.heartbeatMetricsOptions = {
-  logOn: false,
-  breakpoint: null,
-};
+  function heartbeatOn(consumer) {
+    // creates lastHeartbeat, lastHeartbeatDuration, longestHeartbeatDuration properties on consumer.metrics
+    consumer.metrics.lastHeartbeat = 0;
+    consumer.metrics.lastHeartbeatDuration = 0;
+    consumer.metrics.longestHeartbeatDuration = 0;
 
-// this is the event emitter that is running constantly
-heartbeat.heartbeatOn = function (consumer) {
+   // creates logOn and breakpoint properties on consumer.metrics.options
+    consumer.metrics.options.logOn = false;
+    consumer.metrics.options.breakpoint = null;
+  
+    // Methods (manipulate flag properties on consumer.metrics.options for conditional statements in heartbeatOn)
+  
+    // turns on logging every heartbeat
+    consumer.metrics.heartbeatLogOn = function () {
+      consumer.metrics.options.logOn = true;
+      return;
+    };
+    // turns off logging every heartbeat
+    consumer.metrics.heartbeatLogOff = function () {
+      consumer.metrics.options.logOn = false;
+      return;
+    };
+    // creates a breakpoint at the input interval
+    consumer.metrics.heartbeatBreakpoint = function (interval) {
+      consumer.metrics.options.breakpoint = interval;
+      return;
+    };
+    // ends a previously-input breakpoint at the inputted interval
+    consumer.metrics.heartbeatBreakpointOff = function () {
+      consumer.metrics.options.breakpoint = null;
+      return;
+    };
 
-
-  consumer.on("consumer.heartbeat", (e) => {
-    // this console logs the heartbeat timestamp if logOn is turned on
-    if (heartbeat.heartbeatMetricsOptions.logOn) {
-      // consider accessing saved consumer name
-      console.log(`${e.payload.memberId} emits heartbeat at ${e.timestamp}`);
-    }
-
-    //calculate and update metrics:
-    // if heartbeat = 0, set lastHearbeat to equal e.timestamp. skip other updates
-    if (heartbeat.heartbeatMetrics.lastHeartbeat === 0) {
-      heartbeat.heartbeatMetrics.lastHeartbeat = e.timestamp;
-    } else {
-      // calculate lastHeartbeatDuration and update corresponding metric
-      const lastDuration =
-        e.timestamp - heartbeat.heartbeatMetrics.lastHeartbeat;
-      heartbeat.heartbeatMetrics.lastHeartbeatDuration = lastDuration;
-      if (lastDuration > heartbeat.heartbeatMetrics.longestHeartbeatDuration) {
-        heartbeat.heartbeatMetrics.longestHeartbeatDuration = lastDuration;
+    // initializes event emitter for HEARTBEAT
+    consumer.on("consumer.heartbeat", (e) => {
+      // console logs the heartbeat timestamp if logOn is turned on thru logOn method
+      if (consumer.metrics.options.logOn) {
+        // TODO: consider accessing saved consumer name
+        console.log(`${e.payload.memberId} emits heartbeat at ${e.timestamp}`);
       }
-      // updates lastHeartbeat with most recent timestamp
-      heartbeat.heartbeatMetrics.lastHeartbeat = e.timestamp;
-
-      // conditional for sending breakpoint alert
-
-      if (
-        heartbeat.heartbeatMetricsOptions.breakpoint &&
-        heartbeat.heartbeatMetrics.lastHeartbeatDuration &&
-        heartbeat.heartbeatMetrics.lastHeartbeatDuration >
-          heartbeat.heartbeatMetricsOptions.breakpoint
-      ) {
-        const msExceeded =
-          lastDuration - heartbeat.heartbeatMetricsOptions.breakpoint;
-        console.log(`ALERT: HEARTBEAT BREAKPOINT EXCEEDED BY ${msExceeded} MS`);
+      //calculate and update metrics:
+  
+      // if this is the first heartbeat, set lastHeartbeat to equal e.timestamp. skip other updates
+      if (consumer.metrics.lastHeartbeat === 0) {
+        consumer.metrics.lastHeartbeat = e.timestamp;
+      } else {
+        // calculates and updates lastHeartbeatDuration
+        const lastDuration = e.timestamp - consumer.metrics.lastHeartbeat;
+        consumer.metrics.lastHeartbeatDuration = lastDuration;
+        if (lastDuration > consumer.metrics.longestHeartbeatDuration) {
+          consumer.metrics.longestHeartbeatDuration = lastDuration;
+        }
+        // updates lastHeartbeat with most recent timestamp
+        consumer.metrics.lastHeartbeat = e.timestamp;
+  
+        // conditional for sending breakpoint alert (if turned on thru breakpointOn method)
+  
+        if (
+          consumer.metrics.options.breakpoint &&
+          consumer.metrics.lastHeartbeatDuration &&
+          consumer.metrics.lastHeartbeatDuration >
+            consumer.metrics.options.breakpoint
+        ) {
+          const msExceeded = lastDuration - consumer.metrics.options.breakpoint;
+          // TODO: Consider accessing saved consumer name 
+          console.log(`ALERT: HEARTBEAT BREAKPOINT EXCEEDED BY ${msExceeded} MS for ${e.payload.memberId}`);
+        }
+  
+        // ADD FUNCTIONALITY TO DISCONNECT EVENT EMITTER IF A DISCONNECT EVENT OCCURS
+        // RESET LAST HEARTBEAT, LASTHEARTBEAT DURATION, LONGEST HEARTBEAT DURATION
       }
+    });
+  }
 
-      // ADD FUNCTIONALITY TO DISCONNECT EVENT EMITTER IF A DISCONNECT EVENT OCCURS
-      // RESET LAST HEARTBEAT, LASTHEARTBEAT DURATION, LONGEST HEARTBEAT DURATION
-    }
-  });
-};
 
-// this method turns on logging every heartbeat
-heartbeat.heartbeatLogOn = function () {
-  heartbeat.heartbeatMetricsOptions.logOn = true;
-  return;
-};
-
-// this method turns off logging every heartbeat
-heartbeat.heartbeatLogOff = function () {
-  heartbeat.heartbeatMetricsOptions.logOn = false;
-  return;
-};
-
-// this method creates a breakpoint at the input interval
-heartbeat.heartbeatBreakpoint = function (interval) {
-  heartbeat.heartbeatMetricsOptions.breakpoint = interval;
-  return;
-};
-
-// this method ends a previously-input breakpoint at the inputted interval
-heartbeat.heartbeatBreakpointOff = function (interval) {
-  heartbeat.heartbeatMetricsOptions.breakpoint = null;
-  return;
-};
 
 module.exports = heartbeatOn;
--- consumer/index.js
+++ consumer/index.js
@@ -5,6 +5,8 @@ const heartbeat = require('./heartbeat');
 function metricize(consumer) {
   // create empty metrics property on consumer
   consumer.metrics = {};
+  // create empty metricsOptions property for "flag" properties (useful to library, not relevant to developer)
+  consumer.metricsOptions = {};
   // run functions to create metrics for consumer instrumentation events
   endBatchProcess(consumer);
   heartbeat(consumer);
