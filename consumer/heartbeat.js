467e27c991e6ebe5388b1e8843de0582d1fbaf3a
Rebecca Anderson <randersonviolin@gmail.com>
2023-02-27 15:11:01 -0500

    working heartbeat functionality with heartbeat obj export

--- consumer/heartbeat.js
+++ consumer/heartbeat.js
@@ -1,53 +1,58 @@
 const heartbeat = {};
 
-// This contains the data that we want the developer to access
 heartbeat.heartbeatMetrics = {
   lastHeartbeat: 0,
   lastHeartbeatDuration: 0,
   longestHeartbeatDuration: 0,
 };
 
-//
 heartbeat.heartbeatMetricsOptions = {
   logOn: false,
   breakpoint: null,
 };
 
 // this is the event emitter that is running constantly
-heartbeat.heartbeatMetrics = function () {
+heartbeat.heartbeatOn = function (consumer) {
+
+
   consumer.on("consumer.heartbeat", (e) => {
     // this console logs the heartbeat timestamp if logOn is turned on
     if (heartbeat.heartbeatMetricsOptions.logOn) {
       // consider accessing saved consumer name
       console.log(`${e.payload.memberId} emits heartbeat at ${e.timestamp}`);
     }
-    // this console logs the breakpoint alert if breakpoint alert has been turned on AND has been exceeded
 
-    const lastDuration = e.timestamp - heartbeat.heartbeatMetrics.lastHeartbeat;
-    if (
-      heartbeat.heartbeatMetricsOptions.breakpoint &&
-      heartbeat.heartbeatMetricsOptions.lastHeartbeatDuration
-    ) {
+    //calculate and update metrics:
+    // if heartbeat = 0, set lastHearbeat to equal e.timestamp. skip other updates
+    if (heartbeat.heartbeatMetrics.lastHeartbeat === 0) {
+      heartbeat.heartbeatMetrics.lastHeartbeat = e.timestamp;
+    } else {
+      // calculate lastHeartbeatDuration and update corresponding metric
+      const lastDuration =
+        e.timestamp - heartbeat.heartbeatMetrics.lastHeartbeat;
+      heartbeat.heartbeatMetrics.lastHeartbeatDuration = lastDuration;
+      if (lastDuration > heartbeat.heartbeatMetrics.longestHeartbeatDuration) {
+        heartbeat.heartbeatMetrics.longestHeartbeatDuration = lastDuration;
+      }
+      // updates lastHeartbeat with most recent timestamp
+      heartbeat.heartbeatMetrics.lastHeartbeat = e.timestamp;
+
+      // conditional for sending breakpoint alert
+
       if (
-        lastDuration >
-        this.heartbeatBreakpoint.heartbeatMetricsOptions.breakpoint
+        heartbeat.heartbeatMetricsOptions.breakpoint &&
+        heartbeat.heartbeatMetrics.lastHeartbeatDuration &&
+        heartbeat.heartbeatMetrics.lastHeartbeatDuration >
+          heartbeat.heartbeatMetricsOptions.breakpoint
       ) {
         const msExceeded =
           lastDuration - heartbeat.heartbeatMetricsOptions.breakpoint;
-        console.log(
-          `Heartbeat Breakpoint Alert: Breakpoint exceeded by ${msExceeded} ms`
-        );
+        console.log(`ALERT: HEARTBEAT BREAKPOINT EXCEEDED BY ${msExceeded} MS`);
       }
-    }
-    // this updates heartbeatMetrics
-    heartbeat.heartbeatMetrics.lastHearbeat = e.timestamp;
-    heartbeat.lastHeartbeatDuration = lastDuration;
-    if (lastDuration > heartbeat.heartbeatMetrics.longestHeartbeatDuration) {
-      heartbeat.heartbeatMetrics.longestHeartbeatDuration = lastDuration;
-    }
 
-    // ADD FUNCTIONALITY TO DISCONNECT EVENT EMITTER IF A DISCONNECT EVENT OCCURS
-    // RESET LAST HEARTBEAT, LASTHEARTBEAT DURATION, LONGEST HEARTBEAT DURATION
+      // ADD FUNCTIONALITY TO DISCONNECT EVENT EMITTER IF A DISCONNECT EVENT OCCURS
+      // RESET LAST HEARTBEAT, LASTHEARTBEAT DURATION, LONGEST HEARTBEAT DURATION
+    }
   });
 };
 
@@ -63,14 +68,16 @@ heartbeat.heartbeatLogOff = function () {
   return;
 };
 
-// this method creates a breakpoint at the inputted interval
+// this method creates a breakpoint at the input interval
 heartbeat.heartbeatBreakpoint = function (interval) {
   heartbeat.heartbeatMetricsOptions.breakpoint = interval;
   return;
 };
 
-// this method ends a previously-inputted breakpoint at the inputted interval
-heartbeat.heartbeatBreakpoint = function (interval) {
+// this method ends a previously-input breakpoint at the inputted interval
+heartbeat.heartbeatBreakpointOff = function (interval) {
   heartbeat.heartbeatMetricsOptions.breakpoint = null;
   return;
 };
+
+module.exports = heartbeatOn;
--- consumer/index.js
+++ consumer/index.js
@@ -1,10 +1,13 @@
 const endBatchProcess = require('./endBatchProcess');
+const heartbeat = require('./heartbeat');
+
 
 function metricize(consumer) {
   // create empty metrics property on consumer
   consumer.metrics = {};
   // run functions to create metrics for consumer instrumentation events
   endBatchProcess(consumer);
+  heartbeat(consumer);
   return consumer;
 }
 
