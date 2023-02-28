5e1b220df6821bfdb20c9f57d02b29dd8a6673e4
Rebecca Anderson <randersonviolin@gmail.com>
2023-02-27 19:35:01 -0500

    adds disconnect heartbeat metric reset

--- consumer/disconnect.js
+++ consumer/disconnect.js
@@ -4,9 +4,18 @@ function trackConsumerDisconnects(consumer, client) {
     consumer.metrics.isConnected = false;
   }
 }
+
+function resetHeartbeatMetrics(consumer){
+  consumer.metrics.lastHeartbeat = 0;
+  consumer.metrics.lastHeartbeatDuration = 0;
+  // by resetting longestHeartbeatDuration, this becomes a metric just for a single connected session - I think this is most useful? 
+  consumer.metrics.longestHeartbeatDuration = 0;
+}
+
 function consumerDisconnect(consumer, client) {
   consumer.on('consumer.disconnect', () => {
     trackConsumerDisconnects(consumer, client);
+    resetHeartbeatMetrics(consumer);
   });
 }
 
