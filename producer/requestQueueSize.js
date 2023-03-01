233b3d9e1a319e699f9f888e05077c342da35f44
Rebecca Anderson <randersonviolin@gmail.com>
2023-03-01 17:14:14 -0500

    producer console logs updated

--- producer/requestQueueSize.js
+++ producer/requestQueueSize.js
@@ -3,12 +3,29 @@ function requestQueueSize(producer) {
   //when the event is being emitted this function will be invoked
   producer.on('producer.network.request_queue_size', (e) => {
     //1 is happy medium anything higher can cause increase of memory usage and latency so only run when the queue size is larger than 5
-    if(e.payload.queueSize > 5) {
+    if (e.payload.queueSize > 5) {
       currentQueueSizeHistory.push(e.payload.queueSize);
-      console.log(`Current requestQueueSize is : ${e.payload.queueSize}`);
+      // console logs with/without producer name
+      if (producer.metrics.name) {
+        console.log(
+          `Current requestQueueSize is ${e.payload.queueSize} for producer ${producer.metrics.name} (member id: ${producer.metrics.memberId})`
+        );
+      } else {
+        console.log(
+          `Current requestQueueSize is ${e.payload.queueSize} for producer (member id: ${producer.metrics.memberId})`
+        );
+      }
       //alerts the user regarding their queuesize and shows latest history
-      if(producer.metrics.requestQueueSize.logOn && e.payload.queueSize > consumer.metrics.options.requestPendingDuration.breakpoint)
-      console.log(`ALERT: WARNING The requestQueueSize is above 5! Current requestQueueSize is : ${e.payload.queueSize} `, 'current queue size history ->', currentQueueSizeHistory);
+      if (
+        producer.metrics.requestQueueSize.logOn &&
+        e.payload.queueSize >
+          producer.metrics.options.requestPendingDuration.breakpoint
+      )
+        console.log(
+          `ALERT: WARNING The requestQueueSize is above 5! Current requestQueueSize is : ${e.payload.queueSize} `,
+          'current queue size history ->',
+          currentQueueSizeHistory
+        );
     }
   });
 }
