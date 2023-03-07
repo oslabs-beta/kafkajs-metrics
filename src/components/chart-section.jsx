050d7eed0f03904b570a660f2571ea80c639966b
Amanda <eh.beat@yahoo.com>
2023-03-07 08:46:22 -0500

    update charts dynamically based on data from backend

--- src/components/chart-section.jsx
+++ src/components/chart-section.jsx
@@ -6,15 +6,16 @@ class ChartSection extends Component {
 
     render() {
         console.log('in chart section');
+        console.log('props.data', this.props.data);
         return(
             <div>
             <div>Hello, World!</div>
-            <LineChart data = {this.props.data}/>
-            <LineChart data = {this.props.data}/>
-            <LineChart data = {this.props.data}/>
-            <LineChart data = {this.props.data}/>
-            <LineChart data = {this.props.data}/>
-            <LineChart data = {this.props.data}/>
+            <LineChart data = {this.props.data.messagesConsumed}/>
+            <LineChart data = {this.props.data.offSetLag}/>
+            <LineChart data = {this.props.data.lastHeartbeat}/>
+            <LineChart data = {this.props.data.totalRequests}/>
+            <LineChart data = {this.props.data.requestRate}/>
+            <LineChart data = {this.props.data.timeoutRate}/>
             </div>
         );
     }
@@ -23,13 +24,16 @@ class ChartSection extends Component {
         // get data from redis, setState with it
         // then start the interval to get data from redis and setState with it over and over again
         console.log('mount data', this.props.data);
-        setInterval(()=> {
-            const newData = [7, Math.random()*10, Math.random()*10, Math.random()*10, Math.random()*10, 15]
-            this.props.update({datasets: [{
-                label: 'Ummmmm',
-                data: newData,
-            }]});
-        }, 5000);
+        // setInterval(()=> {
+        //     const newData = [7, Math.random()*10, Math.random()*10, Math.random()*10, Math.random()*10, 15]
+        //     this.props.update({datasets: [{
+        //         label: 'Ummmmm',
+        //         data: newData,
+        //     }]});
+        // }, 5000);
+        setInterval(() => {
+            this.props.update();
+        }, 5000)
     }
 }
 
