5409109aaa34eca2f7875fef53a87a776f609be2
Amanda <eh.beat@yahoo.com>
2023-03-09 18:07:31 -0500

    fix reload issue

--- webpack.config.js
+++ webpack.config.js
@@ -2,62 +2,62 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
-    module: {
-        rules: [
-            {
-              test: /\.(js|jsx)$/,
-              exclude: /node_modules/,
-              use: ['babel-loader'],
-            },
-            {
-              test: /.(css|s[ac]ss)$/,
-              use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-            {
-              test: /\.(png|jpe?g|gif)$/i,
-              use: [
-              {
-               loader: 'file-loader',
-              }, ],
-            },
-          ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: './index.html',
-        }),
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /.(css|s[ac]ss)$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          }, ],
+      },
     ],
-    devServer: {
-      headers: { 'Access-Control-Allow-Origin': 'http://localhost:3000/' },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: './index.html',
+    }),
+  ],
+  devServer: {
+    headers: { 'Access-Control-Allow-Origin': 'http://localhost:3000/' },
         
-      static: {
-            directory: path.join(__dirname, './dist'),
+    static: {
+      directory: path.join(__dirname, './dist'),
+    },
+
+    proxy: {
+            
+      '/checktoken': {
+        target: 'http://localhost:3000/',
+        secure: false,
+        changeOrigin: true,
+      },
+      '/getData': {
+        target: 'http://localhost:3000/',
+        secure: false,
+        changeOrigin: true,
+      },
+      '/assets/**': {
+        target: 'http://localhost:3000/',
+        secure: false,
       },
 
-        proxy: {
-            
-            '/checktoken': {
-              target: 'http://localhost:3000/',
-              secure: false,
-              changeOrigin: true,
-            },
-            '/getData': {
-              target: 'http://localhost:3000/',
-              secure: false,
-              changeOrigin: true,
-            },
-            '/assets/**': {
-              target: 'http://localhost:3000/',
-              secure: false,
-            },
-
     },
- }
+  }
 }
