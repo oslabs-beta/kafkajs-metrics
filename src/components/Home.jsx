83922c37ed2d71d8e460decbd2a15939e5138003
Paul Davey <118549898+pauldavey1@users.noreply.github.com>
2023-03-10 15:13:09 -0500

    update linting for React, simplify stylesheet, complete first two sections of homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,97 +1,97 @@
 import React from 'react';
-import KeyFeatureComponent from "./keyFeatureComponent.jsx"
+import KeyFeatureComponent from './keyFeatureComponent.jsx';
 import howToComponents from './howToComponents.jsx';
 import Footer from './footer.jsx';
 
 export default function Home() {
-    return (
-    <>    
-        <div className='container'>
-        <div className="Content-Container">
-            <div className="contents">
-            <p>Lorem ipsum dolor sit amet, 
-                consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore 
-                eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            </div>
-        <div>
-            <img src="https://d29fhpw069ctt2.cloudfront.net/icon/image/74158/preview.svg" style={{ width: '500px', height: '400px' }} />
+  return (
+    <>
+      <div className="intro">
+        <h2>
+          KafkaJSMetrics is a lightweight JavaScript library providing
+          easy-to-use metrics and live alerts to help monitor the health of your
+          KafkaJS consumers, producers, and admins. It integrates seamlessly
+          with your existing KafkaJS client and is activated with just one line
+          of code.
+        </h2>
+        <div className="codebox">
+          <code className="greyout">
+            const kafka = new Kafka();<br></br>
+            <br></br>
+          </code>
+          <code>
+            <code className="salmon">metricize</code>
+            (kafka);<br></br>
+            <br></br>
+          </code>
+          <code className="greyout">
+            const producer = kafka.producer();<br></br>
+            const consumer = kafka.consumer();
+            <br></br>
+            ...
+          </code>
         </div>
+      </div>
+      <div className="keyFeatures">
+        <h1>Key Features</h1>
+        <div className="keyFeatureCardArea">
+          <KeyFeatureComponent
+            img="https://www.cambridgemaths.org/Images/The-trouble-with-graphs.jpg"
+            name="Event-Based Metrics"
+            desc="Harness the power of KafkaJS's instrumentation events without lifting a finger—KJSM provides a full suite of live-updating metrics as soon as it's run"
+          />
+          <KeyFeatureComponent
+            img="https://www.cambridgemaths.org/Images/The-trouble-with-graphs.jpg"
+            name="Breakpoint Alerts"
+            desc="Monitor your KafkaJS client on your own terms with KJSM's customizable breakpoint alerts"
+          />
+          <KeyFeatureComponent
+            img="https://www.cambridgemaths.org/Images/The-trouble-with-graphs.jpg"
+            name="Console Logs"
+            desc="Keep an eye on your client's health the easy way with optional preconfigured console logs"
+          />
+          <KeyFeatureComponent
+            img="https://www.cambridgemaths.org/Images/The-trouble-with-graphs.jpg"
+            name="Visualization"
+            desc="Watch your consumer metrics in real time with the KJSM visualizer—just plug in a unique ID and spin up your client"
+          />
+          <KeyFeatureComponent
+            img="https://www.cambridgemaths.org/Images/The-trouble-with-graphs.jpg"
+            name="Easy To Use"
+            desc="No need to rewrite your KafkaJS client—just npm install, write one line of code, and KJSM adds metrics for all your consumers, producers, and admins"
+          />
+          <KeyFeatureComponent
+            img="https://www.cambridgemaths.org/Images/The-trouble-with-graphs.jpg"
+            name="Lean and Clean"
+            desc="Just like KafkaJS, KJSM is lightweight, dependency-free, and 100% JavaScript"
+          />
         </div>
-        
-        </div>
-        <div className='keyFeatureContainer'>
-            <div className="keyFeatureContainerContents">
-            <div className="contents">
-            <h1>Key Features</h1>
-            <div className="feature">
-            <KeyFeatureComponent 
-                img="https://www.cambridgemaths.org/Images/The-trouble-with-graphs.jpg"
-                name="Instrumentation Events"
-                desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
-            />
-            <KeyFeatureComponent  
-                img="https://cdn-icons-png.flaticon.com/512/2257/2257295.png"
-                name="Breakpoint Alerts"
-                desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
-            />
-            <KeyFeatureComponent 
-                img="https://www.cambridgemaths.org/Images/The-trouble-with-graphs.jpg"
-                name="Logs"
-                desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
-            />
-            <KeyFeatureComponent  
-                img="https://cdn-icons-png.flaticon.com/512/2257/2257295.png"
-                name="Easy Installation"
-                desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
-            />
-            <KeyFeatureComponent 
-                img="https://www.cambridgemaths.org/Images/The-trouble-with-graphs.jpg"
-                name="CLI Tools"
-                desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
-            />
-            <KeyFeatureComponent  
-                img="https://cdn-icons-png.flaticon.com/512/2257/2257295.png"
-                name="100% Javascript"
-                desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
-            />
-              </div>
-            </div>
-        </div>
-        </div>
-        
-        <div className='howToContainer'>
-            <div className="howToContainerContents">
-            <div className="contents">
+      </div>
+      <div className="howToContainer">
+        <div className="howToContainerContents">
+          <div className="contents">
             <h1>How to download</h1>
             <div className="howToContent">
-            <KeyFeatureComponent 
+              <KeyFeatureComponent
                 name="Instrumentation Events"
                 img="https://cdn-icons-png.flaticon.com/512/2257/2257295.png"
-                desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
-            
-            />
-            <KeyFeatureComponent  
+                desc="Lorem Ipsu is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
+              />
+              <KeyFeatureComponent
                 img="https://www.cambridgemaths.org/Images/The-trouble-with-graphs.jpg"
                 name="Breakpoint Alerts"
                 desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
-            
-            />
-            <KeyFeatureComponent 
+              />
+              <KeyFeatureComponent
                 img="https://cdn-icons-png.flaticon.com/512/2257/2257295.png"
                 name="Logs"
                 desc="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,"
-            
-            />
-              </div>
+              />
             </div>
+          </div>
         </div>
-        </div>
-        <Footer/>
+      </div>
+      <Footer />
     </>
-    )
+  );
 }
